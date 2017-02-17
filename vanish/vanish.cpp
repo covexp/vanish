895a8af2915cf5d5e8a1c4e7b7bec7db7635f445
Henrik Lehtinen <henrik.lehtinen@gmail.com>
2017-02-17 21:01:51 +0200

    Added offset buckets for better accuracy

--- vanish/vanish.cpp
+++ vanish/vanish.cpp
@@ -8,28 +8,58 @@
 using namespace cimg_library;
 using namespace std;
 
+const int MINVAL = 0;
+const int MAXVAL = 255;
 const int WIDTH = 480;
 const int HEIGHT = 480;
 const int FRAMES = 10;
-const int BUCKETSIZE = 32;
-const int BUCKETS = 256 / BUCKETSIZE;
+const int BUCKETSIZE = 8;
+const int BUCKETS = (MAXVAL + 1) / BUCKETSIZE;
 
-string DIRECTORY = "logi";
-string FILESTEM = "salsa (";
-string FILETYPE = ").jpg";
+const string DIRECTORY = "logi";
+const string FILESTEM = "salsa (";
+const string FILETYPE = ").jpg";
+
+struct bucketEntry
+{
+	int id;
+	bool isABucket;
+};
+
+int getABucket(int value)
+{
+	if (value < MINVAL)
+		return 0;
+
+	if (value > MAXVAL)
+		return BUCKETS - 1;
+
+	return value / BUCKETSIZE;
+}
+
+int getBBucket(int value)
+{
+	return getABucket(value + (BUCKETSIZE / 2));
+}
 
 int main(void)
 {
-	cout << "Vanishing... " << endl;
+	cout << "Vanish - Version 0.01" << endl;
 
 	CImg<unsigned char> visu(WIDTH, HEIGHT, 1, 1, 0);
-	CImgDisplay main_disp(WIDTH, HEIGHT, "Everything must go");
+	CImgDisplay main_disp(WIDTH, HEIGHT, "Reconstructed background");
 
-	int *buckets = new int[WIDTH * HEIGHT * BUCKETS]();
-	int *finalBucket = new int[WIDTH * HEIGHT]();
+	// Buckets store the number of times in the image sequence that the pixel has a value that falls in the given range, or "bucket"
+	// A-buckets span the whole range from 0 to 255
+	// B-buckets are offset from A-buckets by half of the bucket size and cover a subset of the whole color range
+	//
+	// The motivation for having two sets of buckets are cases when the pixel values cluster around a bucket boundary and would be split in two.
+	// Having an additional set of buckets which are centered around these boundaries helps catch these values and keeps them together.
+	int *valueBucketA = new int[WIDTH * HEIGHT * BUCKETS]();
+	int *valueBucketB = new int[WIDTH * HEIGHT * BUCKETS]();
+	bucketEntry *finalBucket = new bucketEntry[WIDTH * HEIGHT];
 
 	// Read image frames and count the buckets
-
 	for (int frame = 0; frame < FRAMES; frame++)
 	{
 		string filename = DIRECTORY + "/" + FILESTEM + to_string(1 + frame) + FILETYPE;
@@ -42,8 +72,12 @@ int main(void)
 			for (int j = 0; j < HEIGHT; j++)
 			{
 				int pixel = newImage(i, j, 0, 0);
-				int bucket = pixel / BUCKETSIZE;
-				buckets[i + j * WIDTH + bucket * (WIDTH * HEIGHT)]++;
+
+				int a_bucket = getABucket(pixel);
+				valueBucketA[i + j * WIDTH + a_bucket * (WIDTH * HEIGHT)]++;
+
+				int b_bucket = getBBucket(pixel);
+				valueBucketB[i + j * WIDTH + b_bucket * (WIDTH * HEIGHT)]++;
 
 				visu(i, j) = 127;
 			}
@@ -57,23 +91,33 @@ int main(void)
 		{
 			int maxCount = 0;
 			int maxBucket = -1;
+			bool maxTypeA = true;
 
 			for (int bucket = 0; bucket < BUCKETS; bucket++)
 			{
-				if (buckets[i + j * WIDTH + bucket * (WIDTH * HEIGHT)] > maxCount)
+				if (valueBucketA[i + j * WIDTH + bucket * (WIDTH * HEIGHT)] > maxCount)
 				{
-					maxCount = buckets[i + j * WIDTH + bucket * (WIDTH * HEIGHT)];
+					maxCount = valueBucketA[i + j * WIDTH + bucket * (WIDTH * HEIGHT)];
 					maxBucket = bucket;
+					maxTypeA = true;
+				}
+				if (valueBucketB[i + j * WIDTH + bucket * (WIDTH * HEIGHT)] > maxCount)
+				{
+					maxCount = valueBucketB[i + j * WIDTH + bucket * (WIDTH * HEIGHT)];
+					maxBucket = bucket;
+					maxTypeA = false;
 				}
 			}
 
-			finalBucket[i + j * WIDTH] = maxBucket;
+			finalBucket[i + j * WIDTH].id = maxBucket;
+			finalBucket[i + j * WIDTH].isABucket = maxTypeA;
 		}
 	}
 
 	int *acc = new int[WIDTH * HEIGHT]();
 	int *count = new int[WIDTH * HEIGHT]();
 
+	// Average out all the pixel values from the biggest bucket
 	for (int frame = 0; frame < FRAMES; frame++)
 	{
 		string filename = DIRECTORY + "/" + FILESTEM + to_string(1 + frame) + FILETYPE;
@@ -87,15 +131,31 @@ int main(void)
 			{
 				int pixel = newImage(i, j, 0, 0);
 
-				if (pixel > (finalBucket[i + j * WIDTH] - 1) * BUCKETSIZE && pixel < (finalBucket[i + j * WIDTH] + 1) * BUCKETSIZE)
+				if (finalBucket[i + j * WIDTH].isABucket)
 				{
-					acc[i + j * WIDTH] += pixel;
-					count[i + j * WIDTH]++;
+					int pixABucket = getABucket(pixel);
+
+					if (pixABucket == finalBucket[i + j * WIDTH].id)
+					{
+						acc[i + j * WIDTH] += pixel;
+						count[i + j * WIDTH]++;
+					}
+				}
+				else
+				{
+					int pixBBucket = getBBucket(pixel);
+
+					if (pixBBucket == finalBucket[i + j * WIDTH].id)
+					{
+						acc[i + j * WIDTH] += pixel;
+						count[i + j * WIDTH]++;
+					}
 				}
 			}
 		}
 	}
 
+	// Paint the final result in a window
 	for (int i = 0; i < WIDTH; i++)
 	{
 		for (int j = 0; j < HEIGHT; j++)
@@ -115,7 +175,7 @@ int main(void)
 	}
 
 
-	delete buckets;
+	delete valueBucketA;
 
 	return 0;
 }
