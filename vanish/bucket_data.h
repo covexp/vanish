c1e787a03d2aeebf02263dfc5531f3124582eb43
Henrik Lehtinen <henrik.lehtinen@gmail.com>
2017-11-21 22:08:27 +0200

    Add files

---
+++ vanish/bucket_data.h
@@ -0,0 +1,35 @@
+// BucketData
+// Holds the bucket data used by the image processor
+
+#pragma once
+
+#include <vector>
+
+template <typename T>
+struct BucketEntry
+{
+	T id;
+    bool isABucket;
+    short int diff;
+};
+
+template <class T>
+class BucketData
+{
+private:
+
+public:
+	BucketData(int width, int height, int buckets)
+	    : bucketA(width * height * buckets),
+	      bucketB(width * height * buckets),
+	      finalBucket(width * height)
+	{}
+
+	~BucketData()
+	{}
+
+	std::vector<T> bucketA;
+	std::vector<T> bucketB;
+	std::vector<BucketEntry<T>> finalBucket;
+};
+
---
+++ vanish/image_processor.h
@@ -0,0 +1,67 @@
+// ImageProcessor
+// Class to handle the processing of the image sequence
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
+
+#include <CImg.h>
+
+#include "bucket_data.h"
+
+namespace cimg = cimg_library;
+
+typedef std::vector<std::vector<float>> vec2d;
+
+class ImageProcessor
+{
+private:
+    int frames;
+    int width;
+    int height;
+    int size;
+    int channels;
+    int depth;
+
+    int minVal;
+    int maxVal;
+    int bucketSize;
+    int buckets;
+
+    float confLevel;
+
+	std::vector<BucketData<unsigned char> *> bucketData;
+
+    std::vector<std::string> fileNames;
+
+    int getABucket(int value);
+    int getBBucket(int value);
+
+    void inferParameters();
+    void initializeData();
+
+    void printPixelInformation(int x, int y);
+    void printImageData();
+
+	void countBuckets();
+	void refineSolution();
+	void findBiggestBucket();
+	void createFinal();
+
+	void firstPass(vec2d &acc, vec2d &total, std::vector<int> &count);
+	void countFailed(vec2d &acc, std::vector<int> &count, std::vector<bool> &cleared, int confFrames, int &failed);
+	void secondPass(vec2d &acc, vec2d &total, std::vector<int> &count, std::vector<bool> &cleared);
+	void drawImages(vec2d &acc, vec2d &total, std::vector<int> count, int confFrames, int &firstPassFail, int &secondPassFail);
+public:
+    ImageProcessor();
+    ~ImageProcessor();
+
+	void setFiles(const std::vector<std::string> &fn);
+	void setBucketSize(int newSize);
+	void setConfidenceLevel(float newConf);
+	void processSequence();
+};
